07f2fa245947c18e4cdc9a228c2b7ad7e8402f88
sun <sleepsun123@163.com>
2021-11-07 13:35:09 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,113 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" v-if="status">
+    <div class="columns is-multiline">
+      <div class="column is-5" v-for="(item,index) in list" :key="item.ApiKey">
+        <div class="box">
+          <h3>{{item.PcName}}</h3>
+          <div v-if="item.Online">{{index}}</div>
+          <div>{{item.CPU.LoadPerc}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   ComputerList
+  // },
+  data(){
+    return{
+      websock: null,
+      status: false,
+      list: [],
+    }
+  },
+  created(){
+    console.log("start")
+    const ip = "127.0.0.1:13002"
+    const path = "server"
+    const token = "manage_sdkjfskldjflksdjf"
+    this.initWebSocket(ip,path,token)
+  },
+  methods: {
+    initWebSocket(ip,path,token){ //初始化weosocket
+      const wsuri = `ws://${ip}/api/${path}?token=${token}`;
+      this.websock = new WebSocket(wsuri);
+      this.websock.onmessage = this.websocketonmessage;
+      this.websock.onopen = this.websocketonopen;
+      this.websock.onerror = this.websocketonerror;
+      this.websock.onclose = this.websocketclose;
+    },
+    websocketonerror(){//连接建立失败重连
+      this.initWebSocket();
+    },
+    websocketonmessage(e){ //数据接收
+      let t = this
+      let content = "offline"
+      let received_msg = JSON.parse(e.data);
+      if (received_msg.content !== "offline") {
+        content = JSON.parse(received_msg.content)
+      }
+      if (Array.isArray(content)) {
+        console.log("array")
+        for (let i = 0; i < content.length; i++) {
+          const element = content[i];
+          t.list = [...t.list, JSON.parse(element)];
+          if (i === content.length -1) {
+            t.status = true
+          }
+        }
+      }else {
+        if (content === "offline") {
+          const sender = received_msg.sender.split("_")[1]
+          t.list = t.list.filter((x) => {
+            if (x.ApiKey == sender) {
+              x.Online = false
+            }
+            return x
+          })
+        }else{
+          if (t.list.length <= 0) {
+            t.list = [...t.list,content]
+            t.status = true
+          }
+          t.list = t.list.map((x) => {
+            if (x.ApiKey === content.ApiKey) {
+              x = content
+            }
+            return x
+          })
+          if (this.checkHas(t.list,content.ApiKey) === 0) {
+            t.list.push(content)
+          }
+        }
+      }
+    },
+    websocketsend(Data){//数据发送
+      this.websock.send(Data);
+    },
+    websocketclose(e){  //关闭
+      console.log('断开连接',e);
+    },
+    checkHas(arr,ApiKey){
+      arr = arr.filter((x)=>{
+        return x.ApiKey === ApiKey
+      })
+      console.log(arr)
+      return arr.length
+    }
+  },
+  unmounted() {
+    this.websock.close()
+    console.log('连接已关闭...')
+  },
+  watch:{
+    list(val) {
+      console.log(val)
+    }
   }
 }
 </script>
@@ -21,6 +119,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+ 
+.columns {
+   flex-wrap: wrap; 
+   justify-content: space-between
+}
+.column.is-5 {
+height: 100%;
 }
 </style>
---
+++ src/components/ComputerList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <section class="content" style="background:#fff;padding-bottom:0">
+      <div class="catalog">
+        <div class="row row-wrap catelist">
+          <div class="col col-50" v-for="(item,index) in CpList" :key="item.ApiKey">
+            <div class="row ca">
+              <div class="col">
+                <h3>{{item.PcName}}</h3>
+                <div>{{index}}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComputerList',
+  props: 'CpList',
+  mounted(){
+    console.log(this.props)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
